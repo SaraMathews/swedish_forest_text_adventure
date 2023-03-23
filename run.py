678dd7ff2af240ed09a5def3ca392d0c26ea92da
SaraMathews <sara.malmstedt@gmail.com>
2023-03-23 10:07:05 +0000

    Add function restart_game and implement it in play_game function

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def play_game():
     # Character name must be chosen before continuing 
     print("Welcome brave adventurer!")
     character_names = ["Bjorn", "Ragnar", "Hilda"]
-    name = input(f"Choose one of the following character names: {','.join(character_names)}:")
+    name = input(f"Choose one of the following character names:{','.join(character_names)}:")
     while name not in character_names:
         print("That's not one of the options. Please try again")
         name = input(f"Choose one of the following character names: {','.join(character_names)}:")
@@ -23,6 +23,22 @@ def play_game():
     are_you_ready = input(f"Are you ready to enter the deep, dark Swedish forest, {name}? (yes/no):")
     while are_you_ready.lower() not in ["yes", "no"]:
         are_you_ready = input("Hmm... was that a yes or a no?")
+
+    if are_you_ready.lower() == "yes":
+        print(f"Well then, {name}! Get ready...")
+        print("You find yourself lost in the Swedish forest...")
+        print("With no memory of how you got there...")
+        print("The cold autumn wind sends shivers down your spine...")
+        print("When you listen closely, you can hear the trees" 
+              "whispering to each other...")
+        print("You can't stay here, you have to find a way out!")
+        print("Or else.. who knows what's lurking in the dark...")
+        print("Good luck, see you on the other side...hopfully.")
+        first_question()
+    else: 
+        print("It's game over before it even begins!")
+        restart_game()
+
 
 def first_question():
     """
@@ -76,5 +92,20 @@ def restart_game():
     Displays a Game Over box and lets the player choose to either restart 
     the game or exit.  
     """
+    restart_game = input("Do you want to restart the game? (yes or no): ")
+    while restart_game.lower() not in ["yes", "no"]:
+        print("Please enter either 'yes' or 'no'.")
+        restart_game = input("Do you want to restart the game? (yes or no): \n")
+    if restart_game.lower() == "yes":
+        play_game()
+    else:
+        print("")
+        print("Bye then!")
+        print("        /\\_/\\   ")
+        print("  ____/ o o  \\  ")
+        print("  /~____   \"  ) ")
+        print("(_____\/    / \n") 
+        exit()
 
 play_game()
+restart_game()
